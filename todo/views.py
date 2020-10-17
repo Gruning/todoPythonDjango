c9f76988db27964b43d040a981ee414eddbd6e81
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-17 14:44:30 -0300

    cap62-creatingUser

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.models import User
 
 def signupuser(request):
-    return render(request,'todo/signupuser.html')
+    if request.method == 'GET':
+         return render(request,'todo/signupuser.html',{'form':UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2'] :
+            user =User.objects.create_user(
+                request.POST['username'],
+                request.POST['password1'],
+                request.POST['password2'])
+            user.save()
+        else:
+            pass
